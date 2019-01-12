f1b18a4e89ea42c0da9211aaa308f8f115fe83bc
Corey Petty <corey@status.im>
2019-01-12 16:58:07 -0500

    added form to ethcontainer

--- src/containers/EthContainer.js
+++ src/containers/EthContainer.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import QRCode from 'qrcode.react'
 import { css } from "glamor"
 import Web3 from 'web3'
+import { Grid, Button, Form } from 'semantic-ui-react'
 
 let ethContainerStyle = css({
     background: '#ffffffff',
@@ -14,6 +15,7 @@ let qrCodeStyle = css({
     justifySelf: 'end',
 })
 
+const donationNetworkID = 1; // make sure donations only go through on this network.
 const donationAddress = "0x0Ab4d7d50f36A168EbA567b07BbB7D1Ad3372A86"; //replace with the address to watch
 const apiKey = "SC1H6JHAK19WC1D3BGV3JWIFD983E7BS58"; //replace with your own key
 let myweb3
@@ -145,17 +147,94 @@ class EthContainer extends Component {
         })
     };
 
+    handleDonate = event => {
+        event.preventDefault();
+        const form = event.target;
+        let donateWei = new myweb3.utils.BN(
+             myweb3.utils.toWei(form.elements["amount"].value, "ether")
+        );
+        let message = myweb3.utils.toHex(form.elements["message"].value);
+        let extraGas = form.elements["message"].value.length * 68;
+    
+        myweb3.eth.net.getId().then(netId => {
+            switch (netId) {
+                case 1:
+                    console.log("Metamask is on mainnet");
+                    break;
+                case 2:
+                    console.log("Metamask is on the deprecated Morden test network.");
+                    break;
+                case 3:
+                    console.log("Metamask is on the ropsten test network.");
+                    break;
+                case 4:
+                    console.log("Metamask is on the Rinkeby test network.");
+                    break;
+                case 42:
+                    console.log("Metamask is on the Kovan test network.");
+                    break;
+                default:
+                    console.log("Metamask is on an unknown network.");
+            }
+            if (netId === donationNetworkID) {
+                return myweb3.eth.getAccounts().then(accounts => {
+                    return myweb3.eth
+                        .sendTransaction({
+                        from: accounts[0],
+                        to: donationAddress,
+                        value: donateWei,
+                        gas: 150000 + extraGas,
+                        data: message
+                    })
+                    .catch(e => {
+                        console.log(e);
+                     });
+                });
+            } else {
+                console.log("no donation allowed on this network");
+                this.setState({
+                    donateenabled: false
+                });
+            }
+        });
+    };
+
     render() {
         return <div className={`${ethContainerStyle}`}>
-            <h4>Privately: Send directly to the donation address</h4>
-            <QRCode
-                className={`${qrCodeStyle}`}
-                renderAs="svg"
-                fgColor="#000000"
-                bgColor="#89e5ff00"
-                value={donationAddress}
-            />
-            <h4>{donationAddress}</h4>
+            <Grid divided='vertically'>
+                <Grid.Row columns={2} className={`${ethContainerStyle}`}>
+                <Grid.Column className={`${ethContainerStyle}`}>
+                    <h4>Privately: Send directly to the donation address</h4>  
+                    <QRCode
+                        className={`${qrCodeStyle}`}
+                        renderAs="svg"
+                        fgColor="#000000"
+                        bgColor="#89e5ff00"
+                        value={donationAddress}
+                    />
+                    <h4>{donationAddress}</h4>
+                </Grid.Column>
+                <Grid.Column className={`${ethContainerStyle}`}>
+                    <div className={`${ethContainerStyle}`}>
+                        <h4 >
+                            {`Publicly: Send a transaction via `}
+                            <a href="https://metamask.io">Metamask</a>
+                            {` with your name (or something else) as a message `} 
+                        </h4>
+                        <h4>All donations with the same address will be added together.</h4>
+                        <Form  onSubmit={this.handleDonate}>
+                            <input
+                                type="text"
+                                placeholder="ETH to donate"
+                                name="amount"
+                            />
+                            <input type="text" placeholder="message" name="message" />
+                            <Button>Send</Button>
+                        </Form>
+                    </div>
+                </Grid.Column>
+                </Grid.Row>
+            </Grid>
         </div>
       }
 }
